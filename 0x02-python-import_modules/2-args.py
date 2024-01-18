8abe10398f032daf906c866def9550d3dbad0536
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2024-01-18 11:13:15 +0100

    update 📍

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+argement = len(sys.argv) - 1
+
+if argement == 1:
+	print ("{} argument:".format(argement))
+elif(argement == 0):
+	print("{} arguments.".format(argement))
+else:
+	print("{} arguments:".format(argement))
+if argement > 0:
+	for i in (range(1, argement  + 1)):
+		print("{}: {}".format(i, sys.argv[i]))
+	
