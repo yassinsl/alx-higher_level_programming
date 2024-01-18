15a4b0011b260165e651d0388a77c9f7f18e0dac
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2024-01-18 19:40:21 +0100

    update 📍

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-if __name__ != "__main__":
-    exit()
-
-print("{:d}".format(a))
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
