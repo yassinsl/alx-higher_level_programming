0dbec22b1178f8da776785054fce47c424ea2c5f
yassinsl <slawiyassin152@gmail.com>
2024-01-20 00:30:40 -0800

    up

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if not sentence:
         return None
-    else :
+    else:
         lenght = len(sentence)
         first = sentence[0]
         return lenght, first
