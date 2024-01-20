7f35b9e1ec0adf8cfe868f5d2bd497a57dfcf536
yassinsl <slawiyassin152@gmail.com>
2024-01-20 00:19:20 -0800

    up

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     for i in my_string:
         if i == 'c' or i == 'C':
             continue
-        else :
+        else:
             new_string += i
     return new_string
