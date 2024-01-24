916cd8c84863f977a9583bc00b86866777972346
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2024-01-24 11:06:26 +0100

    Update 🤡

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -5,6 +5,5 @@
 #
 # (C) 2024 Yassin LAHSSINI, MARROCO
 # -----------------------------------------------------------
-
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda i: number * i, my_list))
+    return list(map(lambda i: i * number, my_list))
